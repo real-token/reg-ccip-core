052d888c8b4fc40c627f2b8c7037387413c5eabb
NGUYEN Quang Chuc <quangchuc91@gmail.com>
2024-03-19 17:19:44 +0100

    feat: use hardhat-dependency-compiler to deploy Chainlink architecture for test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 import "hardhat-deploy";
+import "hardhat-dependency-compiler";
 import networks from "./hardhat.networks";
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -20,7 +21,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.20",
+        version: "0.8.19",
         settings: {
           optimizer: {
             enabled: true,
@@ -30,8 +31,23 @@ const config: HardhatUserConfig = {
       },
     ],
   },
+  dependencyCompiler: {
+    paths: [
+      "@chainlink/contracts/src/v0.8/shared/token/ERC677/LinkToken.sol",
+      "@chainlink/contracts-ccip/src/v0.8/ccip/ARM.sol",
+      "@chainlink/contracts-ccip/src/v0.8/ccip/ARMProxy.sol",
+      "@chainlink/contracts-ccip/src/v0.8/ccip/CommitStore.sol",
+      "@chainlink/contracts-ccip/src/v0.8/ccip/PriceRegistry.sol",
+      "@chainlink/contracts-ccip/src/v0.8/ccip/Router.sol",
+      "@chainlink/contracts-ccip/src/v0.8/ccip/pools/BurnMintTokenPool.sol",
+      "@chainlink/contracts-ccip/src/v0.8/ccip/onRamp/EVM2EVMOnRamp.sol",
+      "@chainlink/contracts-ccip/src/v0.8/ccip/offRamp/EVM2EVMOffRamp.sol",
+    ],
+  },
   namedAccounts: {
     deployer: 0,
+    admin: 1,
+    moderator: 2,
   },
   networks: networks,
   gasReporter: {
