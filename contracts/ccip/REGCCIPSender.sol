e0b129a0b2577527ec30e93bc61241ff2637b3ed
NGUYEN Quang Chuc <quangchuc91@gmail.com>
2024-03-20 10:09:48 +0100

    feat: use upgradeable contract and naming/version convention

--- contracts/ccip/REGCCIPSender.sol
+++ contracts/ccip/REGCCIPSender.sol
@@ -1,56 +1,92 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
-import {OwnerIsCreator} from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
 import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/contracts/token/ERC20/IERC20.sol";
 import {REGCCIPErrors} from "../libraries/REGCCIPErrors.sol";
 import {IREGCCIPSender} from "../interfaces/IREGCCIPSender.sol";
 
-contract REGCCIPSender is OwnerIsCreator, IREGCCIPSender {
+contract REGCCIPSender is
+    AccessControlUpgradeable,
+    UUPSUpgradeable,
+    IREGCCIPSender
+{
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+
     // Mapping to keep track of allowlisted destination chains.
     mapping(uint64 => bool) public allowlistedChains;
 
-    IRouterClient private s_router;
+    IRouterClient private _router;
 
-    IERC20 private s_linkToken;
+    IERC20 private _linkToken;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
 
     /// @notice Constructor initializes the contract with the router address.
-    /// @param _router The address of the router contract.
-    /// @param _link The address of the link contract.
-    constructor(address _router, address _link) {
-        s_router = IRouterClient(_router);
-        s_linkToken = IERC20(_link);
+    /// @param defaultAdmin The address of the default admin.
+    /// @param upgrader The address of the upgrader.
+    /// @param router The address of the router contract.
+    /// @param link The address of the link contract.
+    function initialize(
+        address defaultAdmin,
+        address upgrader,
+        address router,
+        address link
+    ) public initializer {
+        __AccessControl_init();
+        __UUPSUpgradeable_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(UPGRADER_ROLE, upgrader);
+
+        _router = IRouterClient(router);
+        _linkToken = IERC20(link);
+    }
+
+    /**
+     * @notice The admin (with upgrader role) uses this function to update the contract
+     * @dev This function is always needed in future implementation contract versions, otherwise, the contract will not be upgradeable
+     * @param newImplementation is the address of the new implementation contract
+     **/
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyRole(UPGRADER_ROLE) {
+        // Intentionally left blank
     }
 
     /// @dev Modifier that checks if the chain with the given destinationChainSelector is allowlisted.
-    /// @param _destinationChainSelector The selector of the destination chain.
-    modifier onlyAllowlistedChain(uint64 _destinationChainSelector) {
-        if (!allowlistedChains[_destinationChainSelector])
+    /// @param destinationChainSelector The selector of the destination chain.
+    modifier onlyAllowlistedChain(uint64 destinationChainSelector) {
+        if (!allowlistedChains[destinationChainSelector])
             revert REGCCIPErrors.DestinationChainNotAllowlisted(
-                _destinationChainSelector
+                destinationChainSelector
             );
         _;
     }
 
     /// @dev Modifier that checks the receiver address is not 0.
-    /// @param _receiver The receiver address.
-    modifier validateReceiver(address _receiver) {
-        if (_receiver == address(0))
+    /// @param receiver The receiver address.
+    modifier validateReceiver(address receiver) {
+        if (receiver == address(0))
             revert REGCCIPErrors.InvalidReceiverAddress();
         _;
     }
 
     /// @dev Updates the allowlist status of a destination chain for transactions.
     /// @notice This function can only be called by the owner.
-    /// @param _destinationChainSelector The selector of the destination chain to be updated.
+    /// @param destinationChainSelector The selector of the destination chain to be updated.
     /// @param allowed The allowlist status to be set for the destination chain.
     function allowlistDestinationChain(
-        uint64 _destinationChainSelector,
+        uint64 destinationChainSelector,
         bool allowed
-    ) external onlyOwner {
-        allowlistedChains[_destinationChainSelector] = allowed;
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        allowlistedChains[destinationChainSelector] = allowed;
     }
 
     /// @notice Transfer tokens to receiver on the destination chain.
@@ -58,64 +94,58 @@ contract REGCCIPSender is OwnerIsCreator, IREGCCIPSender {
     /// @notice the token must be in the list of supported tokens.
     /// @notice This function can only be called by the owner.
     /// @dev Assumes your contract has sufficient LINK tokens to pay for the fees.
-    /// @param _destinationChainSelector The identifier (aka selector) for the destination blockchain.
-    /// @param _receiver The address of the recipient on the destination blockchain.
-    /// @param _token token address.
-    /// @param _amount token amount.
+    /// @param destinationChainSelector The identifier (aka selector) for the destination blockchain.
+    /// @param receiver The address of the recipient on the destination blockchain.
+    /// @param token token address.
+    /// @param amount token amount.
     /// @return messageId The ID of the message that was sent.
     function transferTokensPayLINK(
-        uint64 _destinationChainSelector,
-        address _receiver,
-        address _token,
-        uint256 _amount
+        uint64 destinationChainSelector,
+        address receiver,
+        address token,
+        uint256 amount
     )
         external
-        onlyOwner
-        onlyAllowlistedChain(_destinationChainSelector)
-        validateReceiver(_receiver)
+        onlyRole(DEFAULT_ADMIN_ROLE)
+        onlyAllowlistedChain(destinationChainSelector)
+        validateReceiver(receiver)
         returns (bytes32 messageId)
     {
         // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
         //  address(linkToken) means fees are paid in LINK
         Client.EVM2AnyMessage memory evm2AnyMessage = _buildCCIPMessage(
-            _receiver,
-            _token,
-            _amount,
-            address(s_linkToken)
+            receiver,
+            token,
+            amount,
+            address(_linkToken)
         );
 
         // Get the fee required to send the message
-        uint256 fees = s_router.getFee(
-            _destinationChainSelector,
-            evm2AnyMessage
-        );
+        uint256 fees = _router.getFee(destinationChainSelector, evm2AnyMessage);
 
-        if (fees > s_linkToken.balanceOf(address(this)))
+        if (fees > _linkToken.balanceOf(address(this)))
             revert REGCCIPErrors.NotEnoughBalance(
-                s_linkToken.balanceOf(address(this)),
+                _linkToken.balanceOf(address(this)),
                 fees
             );
 
         // approve the Router to transfer LINK tokens on contract's behalf. It will spend the fees in LINK
-        s_linkToken.approve(address(s_router), fees);
+        _linkToken.approve(address(_router), fees);
 
         // approve the Router to spend tokens on contract's behalf. It will spend the amount of the given token
-        IERC20(_token).approve(address(s_router), _amount);
+        IERC20(token).approve(address(_router), amount);
 
         // Send the message through the router and store the returned message ID
-        messageId = s_router.ccipSend(
-            _destinationChainSelector,
-            evm2AnyMessage
-        );
+        messageId = _router.ccipSend(destinationChainSelector, evm2AnyMessage);
 
         // Emit an event with message details
         emit TokensTransferred(
             messageId,
-            _destinationChainSelector,
-            _receiver,
-            _token,
-            _amount,
-            address(s_linkToken),
+            destinationChainSelector,
+            receiver,
+            token,
+            amount,
+            address(_linkToken),
             fees
         );
 
@@ -128,57 +158,54 @@ contract REGCCIPSender is OwnerIsCreator, IREGCCIPSender {
     /// @notice the token must be in the list of supported tokens.
     /// @notice This function can only be called by the owner.
     /// @dev Assumes your contract has sufficient native gas like ETH on Ethereum or MATIC on Polygon.
-    /// @param _destinationChainSelector The identifier (aka selector) for the destination blockchain.
-    /// @param _receiver The address of the recipient on the destination blockchain.
-    /// @param _token token address.
-    /// @param _amount token amount.
+    /// @param destinationChainSelector The identifier (aka selector) for the destination blockchain.
+    /// @param receiver The address of the recipient on the destination blockchain.
+    /// @param token token address.
+    /// @param amount token amount.
     /// @return messageId The ID of the message that was sent.
     function transferTokensPayNative(
-        uint64 _destinationChainSelector,
-        address _receiver,
-        address _token,
-        uint256 _amount
+        uint64 destinationChainSelector,
+        address receiver,
+        address token,
+        uint256 amount
     )
         external
-        onlyOwner
-        onlyAllowlistedChain(_destinationChainSelector)
-        validateReceiver(_receiver)
+        onlyRole(DEFAULT_ADMIN_ROLE)
+        onlyAllowlistedChain(destinationChainSelector)
+        validateReceiver(receiver)
         returns (bytes32 messageId)
     {
         // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
         // address(0) means fees are paid in native gas
         Client.EVM2AnyMessage memory evm2AnyMessage = _buildCCIPMessage(
-            _receiver,
-            _token,
-            _amount,
+            receiver,
+            token,
+            amount,
             address(0)
         );
 
         // Get the fee required to send the message
-        uint256 fees = s_router.getFee(
-            _destinationChainSelector,
-            evm2AnyMessage
-        );
+        uint256 fees = _router.getFee(destinationChainSelector, evm2AnyMessage);
 
         if (fees > address(this).balance)
             revert REGCCIPErrors.NotEnoughBalance(address(this).balance, fees);
 
         // approve the Router to spend tokens on contract's behalf. It will spend the amount of the given token
-        IERC20(_token).approve(address(s_router), _amount);
+        IERC20(token).approve(address(_router), amount);
 
         // Send the message through the router and store the returned message ID
-        messageId = s_router.ccipSend{value: fees}(
-            _destinationChainSelector,
+        messageId = _router.ccipSend{value: fees}(
+            destinationChainSelector,
             evm2AnyMessage
         );
 
         // Emit an event with message details
         emit TokensTransferred(
             messageId,
-            _destinationChainSelector,
-            _receiver,
-            _token,
-            _amount,
+            destinationChainSelector,
+            receiver,
+            token,
+            amount,
             address(0),
             fees
         );
@@ -189,29 +216,26 @@ contract REGCCIPSender is OwnerIsCreator, IREGCCIPSender {
 
     /// @notice Construct a CCIP message.
     /// @dev This function will create an EVM2AnyMessage struct with all the necessary information for tokens transfer.
-    /// @param _receiver The address of the receiver.
-    /// @param _token The token to be transferred.
-    /// @param _amount The amount of the token to be transferred.
-    /// @param _feeTokenAddress The address of the token used for fees. Set address(0) for native gas.
+    /// @param receiver The address of the receiver.
+    /// @param token The token to be transferred.
+    /// @param amount The amount of the token to be transferred.
+    /// @param feeTokenAddress The address of the token used for fees. Set address(0) for native gas.
     /// @return Client.EVM2AnyMessage Returns an EVM2AnyMessage struct which contains information for sending a CCIP message.
     function _buildCCIPMessage(
-        address _receiver,
-        address _token,
-        uint256 _amount,
-        address _feeTokenAddress
+        address receiver,
+        address token,
+        uint256 amount,
+        address feeTokenAddress
     ) private pure returns (Client.EVM2AnyMessage memory) {
         // Set the token amounts
         Client.EVMTokenAmount[]
             memory tokenAmounts = new Client.EVMTokenAmount[](1);
-        tokenAmounts[0] = Client.EVMTokenAmount({
-            token: _token,
-            amount: _amount
-        });
+        tokenAmounts[0] = Client.EVMTokenAmount({token: token, amount: amount});
 
         // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
         return
             Client.EVM2AnyMessage({
-                receiver: abi.encode(_receiver), // ABI-encoded receiver address
+                receiver: abi.encode(receiver), // ABI-encoded receiver address
                 data: "", // No data
                 tokenAmounts: tokenAmounts, // The amount and type of token being transferred
                 extraArgs: Client._argsToBytes(
@@ -219,7 +243,7 @@ contract REGCCIPSender is OwnerIsCreator, IREGCCIPSender {
                     Client.EVMExtraArgsV1({gasLimit: 0})
                 ),
                 // Set the feeToken to a feeTokenAddress, indicating specific asset will be used for fees
-                feeToken: _feeTokenAddress
+                feeToken: feeTokenAddress
             });
     }
 
@@ -231,8 +255,8 @@ contract REGCCIPSender is OwnerIsCreator, IREGCCIPSender {
     /// @notice Allows the contract owner to withdraw the entire balance of Ether from the contract.
     /// @dev This function reverts if there are no funds to withdraw or if the transfer fails.
     /// It should only be callable by the owner of the contract.
-    /// @param _beneficiary The address to which the Ether should be transferred.
-    function withdraw(address _beneficiary) public onlyOwner {
+    /// @param beneficiary The address to which the Ether should be transferred.
+    function withdraw(address beneficiary) public onlyRole(DEFAULT_ADMIN_ROLE) {
         // Retrieve the balance of this contract
         uint256 amount = address(this).balance;
 
@@ -240,31 +264,31 @@ contract REGCCIPSender is OwnerIsCreator, IREGCCIPSender {
         if (amount == 0) revert REGCCIPErrors.NothingToWithdraw();
 
         // Attempt to send the funds, capturing the success status and discarding any return data
-        (bool sent, ) = _beneficiary.call{value: amount}("");
+        (bool sent, ) = beneficiary.call{value: amount}("");
 
         // Revert if the send failed, with information about the attempted transfer
         if (!sent)
             revert REGCCIPErrors.FailedToWithdrawEth(
                 msg.sender,
-                _beneficiary,
+                beneficiary,
                 amount
             );
     }
 
     /// @notice Allows the owner of the contract to withdraw all tokens of a specific ERC20 token.
     /// @dev This function reverts with a 'NothingToWithdraw' error if there are no tokens to withdraw.
-    /// @param _beneficiary The address to which the tokens will be sent.
-    /// @param _token The contract address of the ERC20 token to be withdrawn.
+    /// @param beneficiary The address to which the tokens will be sent.
+    /// @param token The contract address of the ERC20 token to be withdrawn.
     function withdrawToken(
-        address _beneficiary,
-        address _token
-    ) public onlyOwner {
+        address beneficiary,
+        address token
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
         // Retrieve the balance of this contract
-        uint256 amount = IERC20(_token).balanceOf(address(this));
+        uint256 amount = IERC20(token).balanceOf(address(this));
 
         // Revert if there is nothing to withdraw
         if (amount == 0) revert REGCCIPErrors.NothingToWithdraw();
 
-        IERC20(_token).transfer(_beneficiary, amount);
+        IERC20(token).transfer(beneficiary, amount);
     }
 }
--- contracts/interfaces/IREG.sol
+++ contracts/interfaces/IREG.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 /// @title Interface of the Real Estate Governance Token
 /// @author @RealToken
